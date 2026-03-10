d1fbda3b27668f98e4dad11cca33fab9073d6e8b
BilalQanoa <bilalqanoa@gmail.com>
2026-03-10 13:42:01 +0200

    odd numbers from 0 to 20

---
+++ script.js
@@ -0,0 +1,10 @@
+// 1. Display odd numbers from 1 to 20,Using a loop,
+//  create code to print all odd numbers
+//  from 1 to 20 in the console.
+
+console.log("Odd numbers from 1 to 20");
+for (var i = 1; i <= 20; i++){
+    if (i % 2 != 0){
+        console.log(i);
+    }
+}
